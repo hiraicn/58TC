1b88d644869585faf18bdd288d8f408db3631e1d
hiraicn <hiraicn@gmail.com>
2016-11-10 10:32:36 +0800

    change bj => wh

--- channel_extact.py
+++ channel_extact.py
@@ -2,8 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 
 
-start_url = 'http://bj.58.com/sale.shtml'
-url_host = 'http://bj.58.com'
+start_url = 'http://wh.58.com/sale.shtml'
+url_host = 'http://wh.58.com'
 
 def get_index_url(url):
     # url = start_url
@@ -17,56 +17,63 @@ def get_index_url(url):
 get_index_url(start_url)
 
 channel_list = '''
-    http://bj.58.com/shouji/
-    http://bj.58.com/shoujihao/
-    http://bj.58.com/tongxunyw/
-    http://bj.58.com/diannao/
-    http://bj.58.com/bijiben/
-    http://bj.58.com/pbdn/
-    http://bj.58.com/diannaopeijian/
-    http://bj.58.com/zhoubianshebei/
-    http://bj.58.com/shuma/
-    http://bj.58.com/shumaxiangji/
-    http://bj.58.com/mpsanmpsi/
-    http://bj.58.com/youxiji/
-    http://bj.58.com/jiadian/
-    http://bj.58.com/dianshiji/
-    http://bj.58.com/ershoukongtiao/
-    http://bj.58.com/xiyiji/
-    http://bj.58.com/bingxiang/
-    http://bj.58.com/binggui/
-    http://bj.58.com/chuang/
-    http://bj.58.com/ershoujiaju/
-    http://bj.58.com/yingyou/
-    http://bj.58.com/yingeryongpin/
-    http://bj.58.com/muyingweiyang/
-    http://bj.58.com/muyingtongchuang/
-    http://bj.58.com/yunfuyongpin/
-    http://bj.58.com/fushi/
-    http://bj.58.com/nanzhuang/
-    http://bj.58.com/fsxiemao/
-    http://bj.58.com/xiangbao/
-    http://bj.58.com/meirong/
-    http://bj.58.com/yishu/
-    http://bj.58.com/shufahuihua/
-    http://bj.58.com/zhubaoshipin/
-    http://bj.58.com/yuqi/
-    http://bj.58.com/tushu/
-    http://bj.58.com/tushubook/
-    http://bj.58.com/wenti/
-    http://bj.58.com/yundongfushi/
-    http://bj.58.com/jianshenqixie/
-    http://bj.58.com/huju/
-    http://bj.58.com/qiulei/
-    http://bj.58.com/yueqi/
-    http://bj.58.com/bangongshebei/
-    http://bj.58.com/diannaohaocai/
-    http://bj.58.com/bangongjiaju/
-    http://bj.58.com/ershoushebei/
-    http://bj.58.com/danche/
-    http://bj.58.com/fzixingche/
-    http://bj.58.com/diandongche/
-    http://bj.58.com/sanlunche/
-    http://bj.58.com/peijianzhuangbei/
-    http://bj.58.com/tiaozao/
+    http://wh.58.com/shouji/
+    http://wh.58.com/shoujihao/
+    http://wh.58.com/tongxunyw/
+    http://wh.58.com/danche/
+    http://wh.58.com/fzixingche/
+    http://wh.58.com/diandongche/
+    http://wh.58.com/sanlunche/
+    http://wh.58.com/peijianzhuangbei/
+    http://wh.58.com/diannao/
+    http://wh.58.com/bijiben/
+    http://wh.58.com/pbdn/
+    http://wh.58.com/diannaopeijian/
+    http://wh.58.com/zhoubianshebei/
+    http://wh.58.com/shuma/
+    http://wh.58.com/shumaxiangji/
+    http://wh.58.com/mpsanmpsi/
+    http://wh.58.com/youxiji/
+    http://wh.58.com/jiadian/
+    http://wh.58.com/dianshiji/
+    http://wh.58.com/ershoukongtiao/
+    http://wh.58.com/xiyiji/
+    http://wh.58.com/bingxiang/
+    http://wh.58.com/binggui/
+    http://wh.58.com/chuang/
+    http://wh.58.com/ershoujiaju/
+    http://wh.58.com/bangongshebei/
+    http://wh.58.com/diannaohaocai/
+    http://wh.58.com/bangongjiaju/
+    http://wh.58.com/ershoushebei/
+    http://wh.58.com/yingyou/
+    http://wh.58.com/yingeryongpin/
+    http://wh.58.com/muyingweiyang/
+    http://wh.58.com/muyingtongchuang/
+    http://wh.58.com/yunfuyongpin/
+    http://wh.58.com/fushi/
+    http://wh.58.com/nanzhuang/
+    http://wh.58.com/fsxiemao/
+    http://wh.58.com/xiangbao/
+    http://wh.58.com/meirong/
+    http://wh.58.com/yishu/
+    http://wh.58.com/shufahuihua/
+    http://wh.58.com/zhubaoshipin/
+    http://wh.58.com/yuqi/
+    http://wh.58.com/tushu/
+    http://wh.58.com/tushubook/
+    http://wh.58.com/wenti/
+    http://wh.58.com/yundongfushi/
+    http://wh.58.com/jianshenqixie/
+    http://wh.58.com/huju/
+    http://wh.58.com/qiulei/
+    http://wh.58.com/yueqi/
+    http://wh.58.com/chengren/
+    http://wh.58.com/nvyongpin/
+    http://wh.58.com/qinglvqingqu/
+    http://wh.58.com/qingquneiyi/
+    http://wh.58.com/chengren/
+    http://wh.58.com/xiaoyuan/
+    http://wh.58.com/ershouqiugou/
+    http://wh.58.com/tiaozao/
 '''
